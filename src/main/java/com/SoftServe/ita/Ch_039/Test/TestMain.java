feaf7a86a922edaa399760d3a18260aa9c13b744
yakutcv <yakutcv@gmail.com>
2016-04-19 20:08:25 +0300

    Homework

--- src/main/java/com/SoftServe/ita/Ch_039/Test/TestMain.java
+++ src/main/java/com/SoftServe/ita/Ch_039/Test/TestMain.java
@@ -5,6 +5,7 @@ import com.SoftServe.ita.Ch_039.Entity.AnalysisType;
 import com.SoftServe.ita.Ch_039.Entity.Patient;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,16 +38,22 @@ public class TestMain {
                         .build())
                 .build();
 
+
+
+
+
+
+
         Analysis analysis1 = Analysis.newAnalysisBuilder()
                 .setType(AnalysisType.ALLERGY)
                 .setDate("03/02/2015 14:15")
-                .setReport("Good")
+                .setReport("bad")
                 .build();
 
         Analysis analysis2 = Analysis.newAnalysisBuilder()
                 .setType(AnalysisType.ALLERGY)
                 .setDate("03/02/2015 14:14")
-                .setReport("Good")
+                .setReport("bad")
                 .build();
 
         List<Analysis> an = new ArrayList<>();
@@ -55,8 +62,8 @@ public class TestMain {
 
 
 
+
         Patient pat2 = Patient.newPatientBuilder()
-                .setStatus(false)
                 .setLastName("Petia")
                 .setName("Vasia")
                 .setBirthDate("12/02/1945")
@@ -64,28 +71,33 @@ public class TestMain {
                 .build();
 
 
+
+       /* analysis2.setPatient(pat2);*/
+
         //create
-     /*   EntityManagerFactory em = Persistence.createEntityManagerFactory("HOSPITAL");
+        EntityManagerFactory em = Persistence.createEntityManagerFactory("HOSPITAL");
         EntityManager manager = em.createEntityManager();
         manager.getTransaction().begin();
-        manager.persist(pat);
+        manager.persist(pat2);
         manager.getTransaction().commit();
         manager.close();
-        em.close();*/
+        em.close();
+
 
 
         //add method 2
-        EntityManager manager1 = Persistence.createEntityManagerFactory("HOSPITAL").createEntityManager();
+        /*EntityManager manager1 = Persistence.createEntityManagerFactory("HOSPITAL").createEntityManager();
         manager1.getTransaction().begin();
         manager1.persist(pat2);
         manager1.getTransaction().commit();
         manager1.close();
 
+        System.out.println(pat2);*/
 
 
 
         //delete
-        /*EntityManagerFactory em = Persistence.createEntityManagerFactory("HOSPITAL");
+       /* EntityManagerFactory em = Persistence.createEntityManagerFactory("HOSPITAL");
         EntityManager manager = em.createEntityManager();
         manager.getTransaction().begin();
         Patient patient = manager.find(Patient.class, (long)1);
@@ -95,22 +107,38 @@ public class TestMain {
         manager.close();
         em.close();*/
 
+        //read
+     /*   EntityManager manager1 = Persistence.createEntityManagerFactory("HOSPITAL").createEntityManager();
+        manager1.getTransaction().begin();
+        Patient p = manager1.find(Patient.class, 2l);
+        Analysis a = manager1.find(Patient.class, 2l).getList().get(0);
+        manager1.remove(a);
+        //manager1.remove(a);
+        //a.setReport("345345");
+        *//*manager1.merge(p);*//*
+        manager1.getTransaction().commit();
+        manager1.close();
+*/
+       /* EntityManager manager1 = Persistence.createEntityManagerFactory("HOSPITAL").createEntityManager();
+        manager1.getTransaction().begin();
+        Patient patient = manager1.find(Patient.class, 1l);
+        System.out.println(patient);
+        *//*System.out.println(patient.getList());*//*
+        manager1.getTransaction().commit();
+        manager1.close();
+*/
+
+
+
 
         //update
       /*  EntityManager man = Persistence.createEntityManagerFactory("HOSPITAL").createEntityManager();
         man.getTransaction().begin();
-        man.merge(pat);
+        patient.setListAnalyzes(an);
+        man.merge(patient);
+        System.out.println(patient);
         man.getTransaction().commit();
         man.close();*/
-
-
-        //read
-        /*EntityManager manager1 = Persistence.createEntityManagerFactory("HOSPITAL").createEntityManager();
-        manager1.getTransaction().begin();
-        Patient patient = manager1.find(Patient.class, (long) 1);
-        System.out.println(patient);
-        manager1.getTransaction().commit();
-        manager1.close();*/
 
         /*java.util.Date date = new java.util.Date();
         DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy");
